38b5fce586edb6570331d23497d7bdf421551adf
pashpashovich <pavel.kosovich2004@gmail.com>
2024-12-25 22:16:34 +0300

    Added news loadind with comments

--- api/src/main/java/ru/clevertec/CommentsServiceApplication.java
+++ api/src/main/java/ru/clevertec/CommentsServiceApplication.java
@@ -3,9 +3,13 @@ package ru.clevertec;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients("ru.clevertec.client")
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class CommentsServiceApplication {
 
     public static void main(String[] args) {
--- core/src/main/java/ru/clevertec/dto/CommentDto.java
+++ core/src/main/java/ru/clevertec/dto/CommentDto.java
@@ -1,16 +1,17 @@
 package ru.clevertec.dto;
 
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class CommentDto {
+    private UUID id;
     private String text;
     private String username;
     private LocalDateTime createdAt;
