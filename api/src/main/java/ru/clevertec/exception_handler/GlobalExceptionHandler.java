33a2969c5874289c2b7980d455c99239fd6a3f3f
pashpashovich <pavel.kosovich2004@gmail.com>
2024-12-25 22:55:23 +0300

    Profiles added

--- api/src/main/java/ru/clevertec/exception_handler/GlobalExceptionHandler.java
+++ api/src/main/java/ru/clevertec/exception_handler/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package ru.clevertec.exception_handler;
 
 import feign.FeignException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.clevertec.api.ApiResponse;
@@ -14,5 +17,14 @@ public class GlobalExceptionHandler {
                 .data("Новость с таким ID не найдена")
                 .status(false)
                 .build();
+    }
+
+    @ExceptionHandler(FeignException.class)
+    public ResponseEntity<ApiResponse<ErrorResponse>> handleFeignException(FeignException ex) {
+        ApiResponse<ErrorResponse> apiResponse = ApiResponse.<ErrorResponse>builder()
+                .message(ex.getMessage())
+                .status(false)
+                .build();
+        return new ResponseEntity<>(apiResponse, HttpStatus.SERVICE_UNAVAILABLE);
     }
 }
