f4714574e306896f136d5085bc2fb6070f5ced49
pashpashovich <pavel.kosovich2004@gmail.com>
2025-01-13 15:18:49 +0300

    Final

--- api/src/main/java/ru/clevertec/security/JwtTokenProvider.java
+++ api/src/main/java/ru/clevertec/security/JwtTokenProvider.java
@@ -2,9 +2,11 @@ package ru.clevertec.security;
 
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile({"dev", "prod"})
 public class JwtTokenProvider {
 
     @Value("${jwt.secret}")
